d3dcc2255a9ca350601fd90bd19ef61dac9bea91
Sheraz Ali <Sherazalibk@gmail.com>
2018-08-01 02:37:37 -0700

    refactored for recycler view adapter

--- app/src/main/java/com/sheraz/ali/shoppingapp/Controller/MainActivity.kt
+++ app/src/main/java/com/sheraz/ali/shoppingapp/Controller/MainActivity.kt
@@ -2,8 +2,10 @@ package com.sheraz.ali.shoppingapp.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import com.sheraz.ali.shoppingapp.Adapters.CategoryRecyclerAdapter
 import com.sheraz.ali.shoppingapp.Adapters.categoryAdapter
 import com.sheraz.ali.shoppingapp.Model.Category
 import com.sheraz.ali.shoppingapp.R
@@ -12,24 +14,22 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : categoryAdapter
+    lateinit var adapter : CategoryRecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-        adapter = categoryAdapter(this, DataService.categories)
+        adapter = CategoryRecyclerAdapter(this, DataService.categories)
 
         categoryListView.adapter = adapter
 
 
-        categoryListView.setOnItemClickListener { adapterView, view, i, l ->
+        val layoutManager = LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManager
+        categoryListView.setHasFixedSize(true)
 
-            val category = DataService.categories[i]
-            Toast.makeText(this, "You cliced on ${category.title} Cell", Toast.LENGTH_LONG).show()
-
-        }
 
     }
 
